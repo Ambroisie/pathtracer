30c48ea021fc8dfaa32962fd3cfa799cb5dd7cf9
Bruno BELANYI <brunobelanyi@gmail.com>
2020-03-16 15:20:13 +0100

    library: light: add SpatialLight trait definition

--- src/light/mod.rs
+++ src/light/mod.rs
@@ -1,8 +1,14 @@
 use super::core::LinearColor;
-use super::Point;
+use super::{Point, Vector};
 
 /// Represent a light in the scene being rendered.
 pub trait Light: std::fmt::Debug {
     /// Get the illumination of that light on that point.
     fn illumination(&self, point: &Point) -> LinearColor;
+}
+
+/// Represent a light which has an abstract position in the scene being rendered.
+pub trait SpatialLight: Light {
+    /// Get a unit vector from the origin to the position of the light, and its distance
+    fn to_source(&self, origin: &Point) -> (Vector, f64);
 }
