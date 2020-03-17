48b3f03d40ab457f1b872b25af3d0d8ec2e132e2
Bruno BELANYI <brunobelanyi@gmail.com>
2020-03-17 13:55:49 +0100

    library: core: color: add Mul-like ops by self

--- src/core/color.rs
+++ src/core/color.rs
@@ -1,4 +1,5 @@
 use derive_more::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{Div, DivAssign, Mul, MulAssign};
 
 #[derive(
     Debug, Clone, PartialEq, Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign,
@@ -27,6 +28,42 @@ impl LinearColor {
 impl Default for LinearColor {
     fn default() -> Self {
         Self::black()
+    }
+}
+
+impl Mul for LinearColor {
+    type Output = LinearColor;
+
+    fn mul(self, other: Self) -> Self::Output {
+        LinearColor {
+            r: self.r * other.r,
+            g: self.g * other.g,
+            b: self.b * other.b,
+        }
+    }
+}
+
+impl MulAssign for LinearColor {
+    fn mul_assign(&mut self, other: Self) {
+        *self = self.clone() * other
+    }
+}
+
+impl Div for LinearColor {
+    type Output = LinearColor;
+
+    fn div(self, other: Self) -> Self::Output {
+        LinearColor {
+            r: self.r / other.r,
+            g: self.g / other.g,
+            b: self.b / other.b,
+        }
+    }
+}
+
+impl DivAssign for LinearColor {
+    fn div_assign(&mut self, other: Self) {
+        *self = self.clone() / other
     }
 }
 
@@ -143,6 +180,34 @@ mod test {
                 b: 0.3,
             }
         )
+    }
+
+    #[test]
+    fn mul_by_color_works() {
+        let lhs = LinearColor::new(0.125, 0.25, 0.0625);
+        let rhs = LinearColor::new(1.0, 0.5, 2.0);
+        assert_eq!(lhs * rhs, LinearColor::new(0.125, 0.125, 0.125))
+    }
+
+    #[test]
+    fn div_by_color_works() {
+        let lhs = LinearColor::new(1.0, 0.5, 0.25);
+        let rhs = LinearColor::new(4.0, 2.0, 1.0);
+        assert_eq!(lhs / rhs, LinearColor::new(0.25, 0.25, 0.25))
+    }
+
+    #[test]
+    fn mulassign_by_color_works() {
+        let mut lhs = LinearColor::new(0.125, 0.25, 0.0625);
+        lhs *= LinearColor::new(1.0, 0.5, 2.0);
+        assert_eq!(lhs, LinearColor::new(0.125, 0.125, 0.125))
+    }
+
+    #[test]
+    fn divassign_by_color_works() {
+        let mut lhs = LinearColor::new(1.0, 0.5, 0.25);
+        lhs /= LinearColor::new(4.0, 2.0, 1.0);
+        assert_eq!(lhs, LinearColor::new(0.25, 0.25, 0.25))
     }
 
     #[test]
