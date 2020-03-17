63feed70c629ace27011fcaf738d0bd0c07402f0
Bruno BELANYI <brunobelanyi@gmail.com>
2020-03-17 19:08:56 +0100

    library: texture: add Texture trait definition

---
+++ src/texture/mod.rs
@@ -0,0 +1,8 @@
+use super::core::LinearColor;
+use super::Point2D;
+
+/// Represent an object's texture.
+pub trait Texture: std::fmt::Debug {
+    /// Get the color at a given texel coordinate
+    fn texel_color(&self, point: Point2D) -> LinearColor;
+}
