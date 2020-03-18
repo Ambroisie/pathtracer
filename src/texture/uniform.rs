eff133abf3750d44d290a85fb6c45ec2f6858f29
Bruno BELANYI <brunobelanyi@gmail.com>
2020-03-18 14:38:26 +0100

    library: texture: uniform: add deserialization

--- src/texture/uniform.rs
+++ src/texture/uniform.rs
@@ -1,9 +1,10 @@
 use super::Texture;
 use crate::core::LinearColor;
 use crate::Point2D;
+use serde::Deserialize;
 
 /// A texture with the same color on all points.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Deserialize)]
 pub struct UniformTexture {
     color: LinearColor,
 }
@@ -34,12 +35,25 @@ mod test {
     fn simple_texture() -> UniformTexture {
         UniformTexture::new(LinearColor::new(0.25, 0.5, 1.))
     }
+
     #[test]
     fn texel_color_works() {
         let texture = simple_texture();
         assert_eq!(
             texture.texel_color(Point2D::origin()),
             LinearColor::new(0.25, 0.5, 1.)
+        )
+    }
+
+    #[test]
+    fn deserialization_works() {
+        let yaml = r#"
+            color: {r: 1.0, g: 0.5, b: 0.25}
+        "#;
+        let texture: UniformTexture = serde_yaml::from_str(yaml).unwrap();
+        assert_eq!(
+            texture,
+            UniformTexture::new(LinearColor::new(1., 0.5, 0.25))
         )
     }
 }
