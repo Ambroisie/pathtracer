83ed6406ac5c2f2fcf55f6e2daa884777ebe2d17
Bruno BELANYI <brunobelanyi@gmail.com>
2020-03-29 21:04:24 +0200

    executable: allow the choice of renderer

--- pathtracer/src/main.rs
+++ pathtracer/src/main.rs
@@ -1,7 +1,17 @@
 use pathtracer::render::Raytracer;
 use pathtracer::scene::Scene;
 use std::path::PathBuf;
+use std::str;
+use structopt::clap::arg_enum;
 use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(Debug)]
+    enum RenderOption {
+        Raytracer,
+        Pathtracer,
+    }
+}
 
 #[derive(StructOpt, Debug)]
 struct Options {
@@ -11,6 +21,15 @@ struct Options {
     /// Output image for the rendered scene.
     #[structopt(short, long, parse(from_os_str), default_value = "scene.png")]
     output: PathBuf,
+    /// Which renderer should be used on the input scene.
+    #[structopt(
+        short,
+        long,
+        possible_values = &RenderOption::variants(),
+        case_insensitive = true,
+        default_value = "Raytracer"
+    )]
+    renderer: RenderOption,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,7 +37,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let f = std::fs::File::open(options.input)?;
 
     let scene: Scene = serde_yaml::from_reader(f)?;
-    let image = Raytracer::new(scene).render();
+    let image = match options.renderer {
+        RenderOption::Raytracer => Raytracer::new(scene).render(),
+        RenderOption::Pathtracer => todo!(),
+    };
 
     image.save(options.output)?;
     Ok(())
